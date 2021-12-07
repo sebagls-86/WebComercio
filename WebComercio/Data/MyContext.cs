55b08b285aa9217583b9975b255feee989c6c427
h-kaname <luis--ao@hotmail.com>
2021-12-07 20:02:00 -0300

    frontLogin

--- WebComercio/Data/MyContext.cs
+++ WebComercio/Data/MyContext.cs
@@ -30,7 +30,7 @@ namespace WebComercio.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-ETLU83T\SQLEXPRESS;Initial Catalog=comercio;Integrated Security=True");
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-L0586C2;Initial Catalog=comercio;Integrated Security=True");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
